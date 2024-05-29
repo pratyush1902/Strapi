50f7b9efca46ea7b1c260b9f566efb380d5a3393
pratyush kumar <pratyushbndm@gmail.com>
2024-05-29 14:16:16 +0530

    commit

---
+++ config/middlewares.js
@@ -0,0 +1,19 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000'], // Allow requests from your Next.js app
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
